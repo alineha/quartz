ba211ee0478153f5a1afb74d88eb537ecc9f0424
Aline Hommerding Amorim <ahamorim@inf.ufrgs.br>
2026-05-24 18:56:48 -0300

    Change baseUrl from alineha.github.io to imkind.im

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "alineha.github.io/quartz",
+    baseUrl: "imkind.im",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
